7216ad760a82677a274487bb5d10c957b64234f5
Marcel <leqram552@gmail.com>
2023-12-11 19:53:03 +0700

    Leqram

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 
 @app.route('/setup_frame', methods=['POST'])
 def SetupFrame():
+    takes = request.form['take']
+    takes = int(takes)
     imageSTR = request.files['frame'].read()
     image_byte = np.fromstring(imageSTR, np.uint8)
     image = cv2.imdecode(image_byte, cv2.IMREAD_UNCHANGED)
@@ -23,26 +25,32 @@ def SetupFrame():
     min_area_threshold = 8640
 
     posisi = []
-    takes = -1
     for contour in contours:
         area = cv2.contourArea(contour)
         if area > min_area_threshold:
             x, y, w, h = cv2.boundingRect(contour)
+            tengah_frame = (x + (w // 2)) + (y + (h // 2))
             if int(h * (1024 / 680) < w):
                 h = int(w / (1024 / 680))
             if int(w / (1024 / 680) < h):
                 w = int(h * (1024 / 680))
+
+                tengah_foto = (x + (w // 2)) + (y + (h // 2))
+                selisih_tengah = tengah_foto - tengah_frame
+                x = x - selisih_tengah
             
 
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            takes += 1
-            x -= 5
 
+            x -= 5
+            y += 2
             w += 10
             posisi.append([[x, y, w]])
 
     del posisi[0]
     posisi = list(reversed(posisi))
+
+    # # biar tidak tabrakan
     # for take in range(0, len(posisi) - 1, 2):
     #     x1, y1, w1 = posisi[take][0]
     #     x2, y2, w2 = posisi[take+1][0]
@@ -51,6 +59,9 @@ def SetupFrame():
     #         if selisih_lebar > 0:
     #             x1 -= selisih_lebar
     #         posisi[take][0][0] = x1
+    if takes == 3:
+        print('sapi')
+        posisi = [ [posisi[i][0], posisi[i + 1][0]] for i in range(0, len(posisi), 2) ]
     data = {"take": takes,
             "position": posisi}
     with open("format.json", "w") as json_file:
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import json
 
 def cek_frame_transparant(image_path):
     image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
-    dimensi = (2400, 3600)
+    dimensi = (579, 863)
     image = cv2.resize(image, dimensi, interpolation = cv2.INTER_NEAREST)
 
     alpha_channel = image[:, :, 3]
@@ -17,36 +17,44 @@ def cek_frame_transparant(image_path):
 
     posisi = []
     center = []
-    takes = -1
+    takes = 3
+
+    takes = int(takes)
     for contour in contours:
         area = cv2.contourArea(contour)
         if area > min_area_threshold:
             x, y, w, h = cv2.boundingRect(contour)
-            center_x = x + (w // 2)
+            tengah_frame = (x + (w // 2)) + (y + (h // 2))
             if int(h * (1024 / 680) < w):
                 h = int(w / (1024 / 680))
             if int(w / (1024 / 680) < h):
                 w = int(h * (1024 / 680))
-            x = center_x - w // 2
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            takes += 1
-            # x -= 5
-            # w += 10
 
+                tengah_foto = (x + (w // 2)) + (y + (h // 2))
+                selisih_tengah = tengah_foto - tengah_frame
+                x = x - selisih_tengah
+            
+
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+            x -= 5
+            y += 2
+            w += 10
             posisi.append([[x, y, w]])
 
-    # for x, y in zip(transparent_pixels[1], transparent_pixels[0]):
-    #     cv2.rectangle(image, (x, y), (x, y), (102, 255, 255), 2)    
     del posisi[0]
     posisi = list(reversed(posisi))
-    for take in range(0, len(posisi) - 1, 2):
-        x1, y1, w1 = posisi[take][0]
-        x2, y2, w2 = posisi[take+1][0]
-        if x1 + w1 > x2 :
-            selisih_lebar = x1 + w1 - x2
-            if selisih_lebar > 0:
-                x1 -= selisih_lebar
-            posisi[take][0][0] = x1
+    # for x, y in zip(transparent_pixels[1], transparent_pixels[0]):
+    #     cv2.rectangle(image, (x, y), (x, y), (102, 255, 255), 2)    
+    # posisi = list(reversed(posisi))
+    # for take in range(0, len(posisi) - 1, 2):
+    #     x1, y1, w1 = posisi[take][0]
+    #     x2, y2, w2 = posisi[take+1][0]
+    #     if x1 + w1 > x2 :
+    #         selisih_lebar = x1 + w1 - x2
+    #         if selisih_lebar > 0:
+    #             x1 -= selisih_lebar
+    #         posisi[take][0][0] = x1
 
         # if y1 + (w1 / (1024 / 680)) > y3:
         #     selisih_tinggi = y1 + ( w1 / (1024 / 680)) - y3
@@ -56,7 +64,12 @@ def cek_frame_transparant(image_path):
 
     data = {"take": takes,
             "position": posisi}
-    print(data)
+
+    print(f"posisi awal: {posisi}")
+
+    posisi = [ [posisi[i][0], posisi[i + 1][0]] for i in range(0, len(posisi), 2) ]
+
+    print(f"posisi baru: {posisi}")
     cv2.imshow("Detected Transparent Area", image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
